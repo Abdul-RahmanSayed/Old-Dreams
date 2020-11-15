ec8c7c89a13762888ab81ebcbe1660fac8ea5457
Sayed, Abdul-Rahman <asayed7@gatech.edu>
2020-11-14 20:30:04 -0500

    added skeleton code for the game and some test images to test out functionality

---
+++ js/game.js
@@ -0,0 +1,6 @@
+//Creation of canvas with default 600x800 size
+let game = document.createElement("canvas");
+let gameContent = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
---
+++ js/player.js
@@ -0,0 +1,6 @@
+class Player {
+    constructor(sprite, gameWidth, gameHeight) {
+        sprite = new Image();
+        sprite.src = "images/hero.png"
+    }
+}
