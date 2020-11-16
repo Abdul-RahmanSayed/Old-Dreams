425560033746c3e667bd9ca57f2a4958f0335dc9
Sayed, Abdul-Rahman <asayed7@gatech.edu>
2020-11-16 16:36:07 -0500

    added code for default map and character state

--- js/game.js
+++ js/game.js
@@ -1,6 +1,18 @@
-//Creation of canvas with default 600x800 size
-let game = document.createElement("canvas");
-let gameContent = canvas.getContext("2d");
+import Player from '/js/player.js';
 
+//Creation of canvas with default 600x800 size
+let game = document.getElementById("gameScreen");
+let gameContent = game.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
+
+let bgImage = new Image();
+bgImage.src = "images/background_test.png";
+
+bgImage.onload = () => gameContent.drawImage(bgImage, 0, 0, 800, 600);
+let heroImage = new Image();
+heroImage.src = "images/hero_test.png";
+
+heroImage.onload = () => gameContent.drawImage(heroImage, GAME_WIDTH / 2, GAME_HEIGHT / 2)
+
+
--- js/player.js
+++ js/player.js
@@ -1,6 +1,12 @@
-class Player {
-    constructor(sprite, gameWidth, gameHeight) {
-        sprite = new Image();
-        sprite.src = "images/hero.png"
+export default class Player {
+    constructor(gameWidth, gameHeight) {
+        this.position = {
+            x: gameWidth / 2,
+            y: gameHeight / 2
+        }
+    }
+
+    draw(gameContent, img) {
+        img.onLoad = () => gameContent.drawImage(img, this.position.x, this.position.y);
     }
 }
