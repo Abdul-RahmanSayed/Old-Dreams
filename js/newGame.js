b1908b952a5de0213ca07440dab98660216fd081
Sayed, Abdul-Rahman <asayed7@gatech.edu>
2020-12-01 12:59:48 -0500

    worked on walking animation and physical wall implementation (Does not work yet)
    walking animation does not work b/c null pointer from lines 1&2 in js (the bug I have noticed)

--- js/newGame.js
+++ js/newGame.js
@@ -1,6 +1,6 @@
-var character = document.querySelector(".character");
-var map = document.querySelector(".map");
-
+var character = document.getElementsByClassName("character").namedItem("character");
+var map = document.getElementsByClassName("map").namedItem("map");
+console.log(character);
 //movement constants
 const directions = {
     up: "up",
@@ -24,23 +24,24 @@ var speed = 1;
 
 const movement = () => {
     var pixelSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--pixel-size"));
-    const held_directions = held_directions[0];
-    if (held_directions) {
-        if (held_directions === directions.right) {
+    const held_direction = held_directions[0];
+    if (held_direction) {
+        if (held_direction === directions.right) {
             x += speed;
         }
-        if (held_directions === directions.left) {
+        if (held_direction === directions.left) {
             x -= speed;
         }
-        if (held_directions === directions.up) {
+        if (held_direction === directions.up) {
             y += speed;
         }
-        if (held_directions === directions.down) {
+        if (held_direction === directions.down) {
             y -= speed;
         }
     }
+    character.setAttribute("walking", held_direction ? "true" : "false");
     
-    //in-game walls
+    //Walls
     var leftLimit = 0;
     var rightLimit = (16 * 11)+8;
     var topLimit = 50;
@@ -58,29 +59,26 @@ const movement = () => {
         y = bottomLimit;
     }
 
-    map.style.transform = "translate3d( ${-x*pixelSize+camera_left}px, ${-y*pixelSize+camera_top}px, 0 )";
+    map.style.transform = "translate3d( ${-x*pixelSize+camera_left}px, ${-y*pixelSize+camera_top}px, 0)";
     character.style.transform = "translate3d(${x*pixelSize}px, ${y * pixelSize}px, 0)";
 }
 
-
+const main = () => {
+    movement();
+    window.requestAnimationFrame(() => main())
+}
+main();
 
 document.addEventListener("keydown", (ke) => {
-    var direction = keys[ke.key];
+    var direction = keys[ke.which];
     if (direction && held_directions.indexOf(direction) === -1) {
         held_directions.unshift(direction);
     }
 })
 
-document.addEventListener("keydown", (ke) => {
-    var direction = keys[ke.key];
+document.addEventListener("keyup", (ke) => {
+    var direction = keys[ke.which];
     if (held_directions.indexOf(direction) > -1) {
         held_directions.splice(held_directions.indexOf(direction), 1);
     }
-})
-
-
-const main = () => {
-    placeCharacter();
-    window.requestAnimationFrame(() => main());
-}
-main();
+});
